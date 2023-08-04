f35532823b31d924b908d5561f6e4deca462760f
Faruk Bruncevic <bruncevicfaruk@gmail.com>
2023-08-04 19:57:40 +0200

    Added winston for logging

---
+++ src/config/logger.js
@@ -0,0 +1,28 @@
+import winston from 'winston';
+import env from './config.js';
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const logger = winston.createLogger({
+  level: env.env === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    env.env === 'development'
+      ? winston.format.colorize()
+      : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`),
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error'],
+    }),
+  ],
+});
+
+export default logger;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from './app.js';
 import env from './config/config.js';
+import logger from './config/logger.js';
 
 app.listen(env.port, () => {
-  console.log(`App is running on PORT: ${env.port}`);
+  logger.info(`App is running on PORT: ${env.port}`);
 });
