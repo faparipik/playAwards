e1f1ce50698c4b5d9ca6ddf09572e88c989739e3
Faruk Bruncevic <bruncevicfaruk@gmail.com>
2023-08-04 20:05:40 +0200

    Morgan for request loging

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
-
+import morgan from './config/morgan.js';
+import env from './config/config.js';
 const app = express();
+
+if (env.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
 
 // parse json request body
 app.use(express.json());
